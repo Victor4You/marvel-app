8d0b65038114f47a938145b82e3dfa4c4b0219f8
Victor4You <patovm_1998@hotmail.com>
2025-06-30 17:32:46 -0700

    fix: exportar getCharacterById en API client

--- src/api/marvel.js
+++ src/api/marvel.js
@@ -1,9 +1,18 @@
 // src/api/marvel.js
 import axios from 'axios'
+
 const api = axios.create({ baseURL: '/.netlify/functions' })
 
-export function getCharacters(limit, offset) {
+// Listado de personajes
+export function getCharacters(limit = 20, offset = 0) {
   return api
     .get('/getCharacters', { params: { limit, offset } })
     .then(res => res.data.results)
+}
+
+// Detalle de personaje por ID
+export function getCharacterById(id) {
+  return api
+    .get('/getCharacterById', { params: { id } })
+    .then(res => res.data)
 }
