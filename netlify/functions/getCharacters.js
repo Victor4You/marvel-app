0705bf37335a0bd3118695fb5eaf601896c3c409
Victor4You <patovm_1998@hotmail.com>
2025-06-30 21:02:30 -0700

    fix:getCharacter

--- netlify/functions/getCharacters.js
+++ netlify/functions/getCharacters.js
@@ -2,12 +2,21 @@ import axios from 'axios'
 import md5   from 'blueimp-md5'
 
 export async function handler(event) {
+    console.log('▷ PUB_KEY:', process.env.MARVEL_PUBLIC_KEY);
+    console.log('▷ PRIV_KEY:', process.env.MARVEL_PRIVATE_KEY);
+    if (!process.env.MARVEL_PUBLIC_KEY || !process.env.MARVEL_PRIVATE_KEY) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'API keys missing in environment' })
+    };
+  }
   try {
     const { limit = 20, offset = 0 } = event.queryStringParameters || {}
     const ts   = Date.now().toString()
     const PUB  = process.env.MARVEL_PUBLIC_KEY
     const PRIV = process.env.MARVEL_PRIVATE_KEY
     if (!PUB || !PRIV) throw new Error('Missing API keys')
+        
 
     const hash = md5(ts + PRIV + PUB)
     const { data } = await axios.get(
