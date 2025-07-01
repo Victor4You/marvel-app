58128aa7bdb4b502674e847568f75f795f22ea5f
Victor4You <patovm_1998@hotmail.com>
2025-07-01 00:04:26 -0700

    fix api marvel.js

--- src/api/marvel.js
+++ src/api/marvel.js
@@ -1,20 +1,20 @@
 // src/api/marvel.js
-import axios from 'axios'
+ import axios from 'axios'
 
-const api = axios.create({
-  baseURL: '/.netlify/functions'
-})
+ const api = axios.create({
+   baseURL: '/.netlify/functions'
+ })
 
-// Listado de personajes
-export function getCharacters(limit, offset) {
-  return api
-    .get('/getCharacters', { params: { limit, offset } })
-    .then(res => res.data.results)
-}
+ // Listado de personajes
+ export function getCharacters(limit = 20, offset = 0) {
+   return api
+     .get('/getCharacters', { params: { limit, offset } })
+     .then(res => res.data.results)
+  }
 
-// Detalle de personaje por ID
-export function getCharacterById(id) {
-  return api
-    .get('/getCharacterById', { params: { id } })
-    .then(res => res.data)
-}
+ // Detalle de personaje por ID
+ export function getCharacterById(id) {
+   return api
+     .get('/getCharacterById', { params: { id } })
+     .then(res => res.data)
+ }
