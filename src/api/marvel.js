11e656f2af853ca6ddc5805c1160b3c772ca07fa
Victor4You <patovm_1998@hotmail.com>
2025-06-30 23:11:47 -0700

    fix(api): export getCharacterById for CharacterDetail

--- src/api/marvel.js
+++ src/api/marvel.js
@@ -1,9 +1,16 @@
+// src/api/marvel.js
 import axios from 'axios'
-
 const api = axios.create({ baseURL: '/.netlify/functions' })
 
 export function getCharacters(limit, offset) {
   return api
     .get('/getCharacters', { params: { limit, offset } })
-    .then(res => res.data)
+    .then(res => res.data.results)
+}
+
+// --- AÑADE ESTO: ---
+export function getCharacterById(id) {
+  return api
+    .get('/getCharacterById', { params: { id } })
+    .then(res => res.data)        // o res.data.results[0] si devuelves el array completo
 }
